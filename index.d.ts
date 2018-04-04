ca76f47faa416951e5cc507fcc126ef0f9bc6dd6
Kazuma Fujimaki <fujimaki@eq-inc.jp>
2018-04-04 09:47:43 +0900

    Fix d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,13 @@
-declare class KarmiaStorageAdapterMemoryStorage {
+declare interface KarmiaStorageAdapterStorageInterface {
+    store(key: any, value: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    count(callback?: (error: Error, result: any) => void): Promise<any>;
+    has(key: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    get(key: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    set(key: any, value: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    remove(key: any, callback?: (error: Error, result: any) => void): Promise<any>;
+}
+
+declare class KarmiaStorageAdapterMemoryStorage implements KarmiaStorageAdapterStorageInterface {
     buffer: Array<any>;
     map: object;
     index: number;
@@ -7,25 +16,33 @@ declare class KarmiaStorageAdapterMemoryStorage {
     infinite: boolean;
     size: number;
 
-    store(key: any, value: any, callback?: (error: Error, result?: any) => void): Promise<any>|void;
-    count(callback?: (error: Error, result?: any) => void): Promise<any>|void;
-    has(key: any, callback?: (error: Error, result?: any) => void): Promise<any>|void;
-    get(key: any, callback?: (error: Error, result?: any) => void): Promise<any>|void;
-    key(key: any, value: any, callback?: (error: Error, result?: any) => void): Promise<any>|void;
-    remove(key: any, callback?: (error: Error, result?: any) => void): Promise<any>|void;
+    constructor(options?: object, connection?: object);
+    store(key: any, value: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    count(callback?: (error: Error, result: any) => void): Promise<any>;
+    has(key: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    get(key: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    set(key: any, value: any, callback?: (error: Error, result: any) => void): Promise<any>;
+    remove(key: any, callback?: (error: Error, result: any) => void): Promise<any>;
 }
 
-declare class KarmiaStorageAdapterMemory {
+declare interface KarmiaStorageAdapterInterface {
+    getConnection():object;
+    connect(callback?: (error: Error, result: any) => void): Promise<any>;
+    disconnect(callback?: (error: Error, result: any) => void): Promise<any>;
+    storage(name: string, options?: object): KarmiaStorageAdapterMemoryStorage;
+}
+
+declare class KarmiaStorageAdapterMemory implements KarmiaStorageAdapterInterface {
     config: object;
     infinite: boolean;
     size: number;
-    connection?: object;
+    connection?: object|null|undefined;
 
-    constructor(options: object, connection?: object);
-    getConnection(): object|undefined;
-    connect(callback?: (error: Error, result?: any) => void): Promise<void>|undefined;
-    disconnect(callback?: (error: Error, result?: any) => void): Promise<void>|undefined;
-    getStorage(name: string, options?: object): KarmiaStorageAdapterMemoryStorage;
+    constructor(options?: object, connection?: object);
+    getConnection(): object;
+    connect(callback?: (error: Error, result: any) => void): Promise<any>;
+    disconnect(callback?: (error: Error, result: any) => void): Promise<any>;
+    storage(name: string, options?: object): KarmiaStorageAdapterMemoryStorage;
 }
 
 export = KarmiaStorageAdapterMemory;
